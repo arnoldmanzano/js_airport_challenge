6f69c4d7ce4e7ddb787245e03f72297c1c78aa1d
Apple <Apple@Apples-MacBook-Air.local>
2016-02-16 15:39:48 +0000

    file set-up

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,18 @@
+// I want to instruct a plane to land at an airport and confirm that it has landed
+
+describe('Airport', function(){
+
+  var plane;
+  var airport;
+
+  beforeEach(function() {
+    plane = new Plane();
+    airport = new Airport();
+  });
+
+  it('knows to instruct a plane to land', function() {
+    plane.land(airport);
+    expect(airport.planes()).toContain(plane); 
+  });
+
+});
